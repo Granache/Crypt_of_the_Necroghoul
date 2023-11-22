8bd13532e249aab4bf95a083904316fd718eaf65
Granache <dcfallon04@gmail.com>
2023-11-21 21:54:15 -0500

    Removed bugged code.

--- GianLibrary.py
+++ GianLibrary.py
@@ -9,7 +9,6 @@
 
 import DiaLibrary
 from JadeLibrary import DisplayMenu, MenuChoice, DisplayIntro, DisplayOutro, GetName
-import GianLibrary
 
 
 # 8)
@@ -18,26 +17,14 @@ import GianLibrary
 # Parameters: none
 # Purpose: Allows for user to choose a selection from the menu ranging from 1-3 options
 
+
 # Define MenuChoice function
 def MenuChoice(userChoice):
     # Display the first room layout, with a menu to interact with 1) Treasure, 2)Enemy, 3) Exit
-    print("Loading menu . . .\n")
-    print("1) Treasure")
-    print("2) Enemy")
-    print("3) Exit\n")
-    userChoice = int(input("Please enter your option here: \n"))
     # Create a nested selection structure that allows for interactive results
     # use if, elif, and else statements
     # store choice that user has made
-    if userChoice == 1:
-        print("As you pick up the scroll, a message appears...")
-        print("Between two doors, what you are looking for will appear.")
-    elif userChoice == 2:
-        print("Throughout this journey many obstacles will be thrown your way, will you be able to overcome them?")
-    elif userChoice == 3:
-        return userChoice
-    else:
-        print("Error . . .Please enter a number 1-3")
+    pass
 
 
 # 10)
@@ -58,22 +45,7 @@ def TreasureChoice(doorChoice, chances):
     # Generate random chance that they survive
     # If they survive, print a statement that they overcame the obstacle and managed to get to the treasure
     # Else, print an error statement and to choose through the options of numbers
-    items = ['potions', 'gold coins', 'new weapons']
-    potions = potions
-    goldCoins = goldCoins
-    newWeapons = newWeapons
-    treasure = print(random.choice(items))
-    if treasure == potions:
-        print("Congratulations {name}, you have recieved valuable potions!")
-        print("This can be of use in combat, health, or abilities.")
-    elif treasure == goldCoins:
-        print("Congratulations {name}, you have recieved valuable currency!")
-        print("This will allow for you to buy items to help you moving foward")
-    elif treasure == newWeapons:
-        print("Congratulations {name}, you have recieved new weapons!")
-        print("This will allow you to protect yourself from enemies.")
-    else:
-        print("The treasure has already been found!")
+    pass
 
 
 # 12)
@@ -87,15 +59,7 @@ def PointSystem(points, playerScore, playerHealth):
     # If correct return their victory
     # If they lose, they are not correct, they suffered a lose
     # If they suffered a lose, they lose a point
-    playerScore = 0
-    playerHealth = 100
-    points = 0
-    if playerHealth > 0:
-        random.randint(10, 20) and + playerScore
-        print("You have gained one point !")
-    else:
-        random.randint(10, 20) and - playerScore
-        print("You have lost one point !")
+    pass
 
 
 # 13)
@@ -106,11 +70,8 @@ def PointSystem(points, playerScore, playerHealth):
 # Define usersScore
 def usersScore():
     # string user's score and allow it to be saved as an input
-    userScore = 0
     # Once rounds of the game have finished display the user's score
-    print("*" * 40)
-    print(userScore)
-    print("*" * 40)
+    pass
 
 
 # 14)
@@ -124,23 +85,7 @@ def ScoreBoard():
     # If true, then the score can contine on and be displayed
     # Display a statement congratulating the user
     # If false, then display a statment asking the user to try again
-    print("Loading Score Board . . .\n")
-    print("*" * 40)
-    text_file = open("highscores.txt", "r")
-    with open('highscores.txt', 'r')
-        contents = file.readlines()
-
-    for i in range(len(contents)):
-        contents[i] = contents[i].strip
-    print(contents)
-    whole_thing = text_file.read()
-    print(whole_thing)
-    text_file.close()
-    if userScore > highScore:
-        print(userScore)
-        print("Congratulations {name} !, you have made it onto the offical Score Board.")
-        print("*" * 40)
-
+    pass
 
 
 
